21b89abb6ac548aa1c29db8d76c35dc0a2fabd83
rakshithgowdar25ec-max <rakshithgowdar.25ec@saividya.ac.in>
2026-08-16 20:27:36 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,1 +1,34 @@
+const crypto = require("crypto");
 
+function requireAuth(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+  }
+  return res.redirect("/login");
+}
+
+function requireGuest(req, res, next) {
+  if (req.session && req.session.userId) {
+    return res.redirect("/dashboard");
+  }
+  return next();
+}
+
+function requirePendingTwoFactor(req, res, next) {
+  if (req.session && req.session.pendingUserId) {
+    return next();
+  }
+  return res.redirect("/login");
+}
+
+function ensureCsrfToken(req, res, next) {
+  if (!req.session.csrfToken) {
+    req.session.csrfToken = crypto.randomBytes(32).toString("hex");
+  }
+  res.locals.csrfToken = req.session.csrfToken;
+  next();
+}
+
+function verifyCsrfToken(req, res, next) {
+  const submitted = req.body._csrf;
+  if (!submitted || submitted !== req.session.csrfToken) {
